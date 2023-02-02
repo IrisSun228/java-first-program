b8e01c0ff09f2db9409b177a030b9d7abe6ef150
Iris Sun <irissun@live.com>
2023-02-01 20:21:45 -0500

    Section 4 Step 3 - Create a Command Usage Helper

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -1,8 +1,15 @@
 package com.h2;
 
+import java.util.Map;
+
 public class Finance {
     public static final String BEST_LOAN_RATES = "bestLoanRates";
     public static final String SAVINGS_CALCULATOR = "savingsCalculator";
     public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
+    public static final Map<String, String> commandsToUsage = Map.of(
+            BEST_LOAN_RATES, "usage: bestLoanRates",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+    );
 }
