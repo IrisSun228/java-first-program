8c446e326e9a2905a12cd3f41497ae69061d64cb
Iris Sun <irissun@live.com>
2023-02-01 20:47:40 -0500

    Section 5 Step 3 - Implement getLongValue()

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -2,6 +2,14 @@ package com.h2;
 
 public class Utilities {
     public static long getLongValue(String in) {
-        return 0L;
+        long out = Long.MIN_VALUE;
+
+        try {
+            out = Long.parseLong(in);
+        } catch (NumberFormatException e) {
+            System.out.println(in + " cannot be converted into a 'long' value. Exiting program.");
+        }
+
+        return out;
     }
 }
