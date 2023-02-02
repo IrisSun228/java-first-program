d75736a3881d004082ff2a1ddc4c3d218d416a16
Iris Sun <irissun@live.com>
2023-02-01 20:18:46 -0500

    Section 4 Step 2 - Create Three Constant Members In Class

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -1,4 +1,8 @@
 package com.h2;
 
 public class Finance {
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
 }
