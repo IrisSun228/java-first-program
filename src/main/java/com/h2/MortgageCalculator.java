dd54c99f9bad766bf03b282ebee0ea5af8da4fb0
Iris Sun <irissun@live.com>
2023-02-01 20:13:21 -0500

    Section 3 Step 13 - Complete the Command-Line Application Implementation !SECTION 3 COMPLETE!

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -41,5 +41,15 @@ public class MortgageCalculator {
         return "monthlyPayment: " + df.format(monthlyPayment);
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+
+        calculator.calculateMonthlyPayment();
+
+        System.out.println(calculator.toString());
+    }
 }
